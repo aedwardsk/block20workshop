29c3e8ae2fc40bd6517dd662d84f54a4ce2a7c06
Andy Edwards <andyedwards@outlook.com>
2025-01-14 13:35:04 -0500

    got Initial variables

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // TODO: this file! :)
+// elements needed to work with
+const addNumToSheet = document.querySelector("form");
+const numberInput = document.querySelector('input[name="number"]');
+const sortOneButton = document.querySelector("#sortOne");
+const sortAllButton = document.querySelector("#sortAll");
