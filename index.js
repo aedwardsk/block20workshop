bcd37b9b91229fc21521d5eaac376eb48dc2effb
Andy Edwards <andyedwards@outlook.com>
2025-01-15 14:03:23 -0500

    wrote render function to display text

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-// TODO: this file! :)
-// elements needed to work with
-
-const sortOneButton = document.querySelector("#sortOne");
-const sortAllButton = document.querySelector("#sortAll");
-
 //state
 const state = {
   numBank: [],
   odds: [],
   evens: [],
 };
-//function for addnumToSheet
+//TODO need to write function to display output
+function render() {
+  numBankDisp = document.querySelector("#numberBank output");
+  numBankDisp.textContent = state.numBank.join();
+
+  oddsDisp = document.querySelector("#odds output");
+  oddsDisp.textContent = state.odds.join();
+
+  evensDisp = document.querySelector("#evens output");
+  evensDisp.textContent = state.evens.join();
+}
+
+//TODO need to write sort one function.
+function sortOne() {
+  const sortOneButton = document.querySelector("#sortOne");
+}
+//TODO need to write store function for the sort all odd and even.
+//TODO need to write sort all function.
+function sortAll() {
+  const sortAllButton = document.querySelector("#sortAll");
+}
+//TODO nee to make click events for both sorts
+
+//function complete for adding numbers to the form
 const addNumToSheet = document.querySelector("form");
 addNumToSheet.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -19,10 +36,12 @@ addNumToSheet.addEventListener("submit", (event) => {
   const addingNumber = parseInt(numberInput.value, 10);
 
   if (addingNumber <= 0 || !Number.isInteger(addingNumber)) {
-    return "invalid";
+    alert("Please enter a valid positive integer.");
+    return;
   }
-  for (let i = 0; i < addingNumber; i++) {
-    //may need to change back to i number
-    state.numBank.push(i);
-  }
+  state.numBank.push(addingNumber);
+
+  render();
+  numberInput.value = "";
 });
+//TODO
