e336ba80beb1646b6411c77cb350ba18e76e8b55
Andy Edwards <andyedwards@outlook.com>
2025-01-14 21:57:57 -0500

    got adding numbers to sheet and to the bank

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // TODO: this file! :)
 // elements needed to work with
-const addNumToSheet = document.querySelector("form");
-const numberInput = document.querySelector('input[name="number"]');
+
 const sortOneButton = document.querySelector("#sortOne");
 const sortAllButton = document.querySelector("#sortAll");
 
@@ -11,3 +10,19 @@ const state = {
   odds: [],
   evens: [],
 };
+//function for addnumToSheet
+const addNumToSheet = document.querySelector("form");
+addNumToSheet.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const numberInput = document.querySelector('input[name="number"]');
+  const addingNumber = parseInt(numberInput.value, 10);
+
+  if (addingNumber <= 0 || !Number.isInteger(addingNumber)) {
+    return "invalid";
+  }
+  for (let i = 0; i < addingNumber; i++) {
+    //may need to change back to i number
+    state.numBank.push(i);
+  }
+});
